24831e77a14f4d462b92c4bbaa6df51c53f6e94b
siddhipande3 <siddhipande16@gmail.com>
2024-08-18 10:39:43 +0530

    Update App.java

--- src/main/java/com/GRRAS1/App.java
+++ src/main/java/com/GRRAS1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Welcome  hii guys ! " );
+        System.out.println( "Hello World Welcome  hii guys  This is devops batch ! " );
     }
 }
